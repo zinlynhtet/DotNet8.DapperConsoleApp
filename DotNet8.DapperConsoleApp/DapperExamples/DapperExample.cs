30f6f56418da475d28a4e98be1ee0cfb359d8562
Zin Linn Htet <mackk5504@gmail.com>
2024-04-12 22:49:52 +0630

    Complete Project Frame

---
+++ DotNet8.DapperConsoleApp/DapperExamples/DapperExample.cs
@@ -0,0 +1,24 @@
+﻿namespace DotNet8.DapperConsoleApp.DapperExamples;
+
+public class DapperExample
+{
+    public void Run()
+    {
+
+    }  public void Get()
+    {
+
+    }  public void GetById()
+    {
+
+    }  public void Create()
+    {
+
+    }  public void Update()
+    {
+
+    }  public void Delete()
+    {
+
+    }
+}
