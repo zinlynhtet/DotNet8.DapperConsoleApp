cabd4561f903463c7ec9e3ab503cffc5abb19a07
Zin Linn Htet <mackk5504@gmail.com>
2024-04-15 01:20:08 +0630

    Fix Console write class

--- DotNet8.DapperConsoleApp/DapperExamples/Queries.cs
+++ DotNet8.DapperConsoleApp/DapperExamples/Queries.cs
@@ -2,5 +2,13 @@
 
 public class Queries
 {
+    public static string GetAllQuery { get; } = "Select * from Tbl_Blogs";
 
+    public static void ConsoleWrite(dynamic item)
+    {
+        Console.WriteLine(item.BlogId);
+        Console.WriteLine(item.BlogTitle);
+        Console.WriteLine(item.BlogAuthor);
+        Console.WriteLine(item.BlogContent);
+    }
 }
