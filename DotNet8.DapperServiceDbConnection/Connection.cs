38796e361fae4ddadb8b974bfea4bd27993a5717
Zin Linn Htet <mackk5504@gmail.com>
2024-04-17 21:26:20 +0630

    -

---
+++ DotNet8.DapperServiceDbConnection/Connection.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+namespace DotNet8.DapperServiceDbConnection;
+
+public class Connection
+{
+    public string DapperExample()
+    {
+        var sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+        sqlConnectionStringBuilder.DataSource = ".";
+        sqlConnectionStringBuilder.InitialCatalog = "TestDb";
+        sqlConnectionStringBuilder.UserID = "sa";
+        sqlConnectionStringBuilder.Password = "sasa@123";
+        sqlConnectionStringBuilder.TrustServerCertificate = true;
+        return sqlConnectionStringBuilder.ConnectionString;
+    }
+}
