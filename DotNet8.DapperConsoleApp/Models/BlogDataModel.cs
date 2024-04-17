1ae747b1edaf6f367f9c6ddbafe5fd069abdbe08
Zin Linn Htet <mackk5504@gmail.com>
2024-04-17 21:26:11 +0630

    -

---
+++ DotNet8.DapperConsoleApp/Models/BlogDataModel.cs
@@ -0,0 +1,9 @@
+namespace DotNet8.DapperConsoleApp.Models;
+
+public class BlogDataModel
+{
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+}
