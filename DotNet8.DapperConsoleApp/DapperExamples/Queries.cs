f7060d4e31cf017d455f2b4bce3efedc7ce4a02a
Zin Linn Htet <mackk5504@gmail.com>
2024-04-23 14:38:05 +0630

    Add Sql Queries

--- DotNet8.DapperConsoleApp/DapperExamples/Queries.cs
+++ DotNet8.DapperConsoleApp/DapperExamples/Queries.cs
@@ -3,6 +3,23 @@
 public class Queries
 {
     public static string GetAllQuery { get; } = "Select * from Tbl_Blogs";
+    public static string GetByIdQuery { get; } = @"Select * from Tbl_Blogs where BlogId = @BlogId";
+    public static string CreateQuery { get; } = @"INSERT INTO [dbo].[Tbl_Blogs]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           (@BlogTitle,
+		   @BlogAuthor,
+		   @BlogContent)";
+
+    public static string UpdateQuery { get; } = @"UPDATE [dbo].[Tbl_Blogs]
+     SET [BlogTitle] = @BlogTitle
+      ,[BlogAuthor] = @BlogAuthor
+      ,[BlogContent] = @BlogContent
+     WHERE BlogId = @BlogId";
+    public static string DeleteQuery { get; } = @"DELETE FROM [dbo].[Tbl_Blogs]
+    WHERE BlogId = @BlogId";
 
     public static void ConsoleWrite(dynamic item)
     {
