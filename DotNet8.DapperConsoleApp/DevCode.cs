7eceee1df6d511ee83d1761dcc059c03d9af2189
Zin Linn Htet <mackk5504@gmail.com>
2024-04-23 16:32:43 +0630

    Add DevCodes

---
+++ DotNet8.DapperConsoleApp/DevCode.cs
@@ -0,0 +1,31 @@
+﻿namespace DotNet8.DapperConsoleApp;
+
+public static class DevCode
+{
+    public static void ToConsole(this object item)
+    {
+        dynamic dynamicItem = item;
+
+        if (dynamicItem is not null)
+        {
+            Console.WriteLine($"BlogId: {dynamicItem.BlogId}");
+            Console.WriteLine($"BlogTitle: {dynamicItem.BlogTitle}");
+            Console.WriteLine($"BlogAuthor: {dynamicItem.BlogAuthor}");
+            Console.WriteLine($"BlogContent: {dynamicItem.BlogContent}");
+        }
+        else
+        {
+            Console.WriteLine("Object is null.");
+        }
+    }
+
+    public static string ToJson(this object obj)
+    {
+        return JsonConvert.SerializeObject(obj);
+    }
+
+    public static int ToInt(this string str)
+    {
+        return Convert.ToInt32(str);
+    }
+}
