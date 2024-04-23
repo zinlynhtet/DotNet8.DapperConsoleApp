aa5467e89f96dc4e877504e5d1536c2b0caa98ee
Zin Linn Htet <mackk5504@gmail.com>
2024-04-23 14:37:26 +0630

    Complete Dapper Set Up

--- DotNet8.DapperConsoleApp/DapperExamples/DapperExample.cs
+++ DotNet8.DapperConsoleApp/DapperExamples/DapperExample.cs
@@ -2,48 +2,38 @@
 
 public class DapperExample
 {
-    private string  ;
+    private readonly Connection _connection;
+
+    public DapperExample(Connection connection)
+    {
+        _connection = connection;
+    }
+
     public void Run()
     {
-        Get<BlogDataModel>();
+        
     }
 
-    private void Get<T>()
+    private void Get()
     {
-        string query = Queries.GetAllQuery;
-        using IDbConnection db = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
-        var lst = db.Query<T>(query).ToList();
-        foreach (var item in lst)
-        {
-            Queries.ConsoleWrite(item);
-        }
+       
     }
-    //dynamic can be anything "can be the column name from the database or not". So rarely use the dynamic method.
-    // private void Get()
-    // {
-    //     string query = Queries.GetAllQuery;
-    //     using IDbConnection db = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
-    //     var lst = db.Query(query).ToList();
-    //     foreach (var item in lst)
-    //     {
-    //         Queries.ConsoleWrite(item);
-    //     }
-    // }
+
+    private void GetById(int id)
+    {
+       
+    }
 
 
-    public void GetById()
+    public void Create(BlogDataModel reqModel)
     {
     }
 
-    public void Create()
+    public void Update(BlogDataModel reqModel)
     {
     }
 
-    public void Update()
-    {
-    }
-
-    public void Delete()
+    public void Delete(int id)
     {
     }
 }
