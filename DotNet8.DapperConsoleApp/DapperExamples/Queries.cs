b6d808d96b17a8460083ef44385cd32f746f86e5
Zin Linn Htet <mackk5504@gmail.com>
2024-04-23 16:33:14 +0630

    Add Queries

--- DotNet8.DapperConsoleApp/DapperExamples/Queries.cs
+++ DotNet8.DapperConsoleApp/DapperExamples/Queries.cs
@@ -4,6 +4,7 @@ public class Queries
 {
     public static string GetAllQuery { get; } = "Select * from Tbl_Blogs";
     public static string GetByIdQuery { get; } = @"Select * from Tbl_Blogs where BlogId = @BlogId";
+
     public static string CreateQuery { get; } = @"INSERT INTO [dbo].[Tbl_Blogs]
            ([BlogTitle]
            ,[BlogAuthor]
@@ -18,14 +19,7 @@ public class Queries
       ,[BlogAuthor] = @BlogAuthor
       ,[BlogContent] = @BlogContent
      WHERE BlogId = @BlogId";
+
     public static string DeleteQuery { get; } = @"DELETE FROM [dbo].[Tbl_Blogs]
     WHERE BlogId = @BlogId";
-
-    public static void ConsoleWrite(dynamic item)
-    {
-        Console.WriteLine(item.BlogId);
-        Console.WriteLine(item.BlogTitle);
-        Console.WriteLine(item.BlogAuthor);
-        Console.WriteLine(item.BlogContent);
-    }
 }
